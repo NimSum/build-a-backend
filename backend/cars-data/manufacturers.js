9775b6b7f2d22cf79e5d0fb58c3917ff7f68035e
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-25 19:44:43 -0600

    Create map through cars to connect manuf id to manufacturers

--- backend/cars-data/manufacturers.js
+++ backend/cars-data/manufacturers.js
@@ -1,15 +1,26 @@
 let cars = require('./cars.json');
+let manufs = require('./manufacturers.json');
 let fs = require('fs');
 
-const cleaned = cars.reduce((acc, car) => {
-  acc[car.manufacturer] = null;
-  return acc;
-}, {});
+
+// Map through to connect id to manufacturer
+const manufss = cars.map(car => {
+  const found = manufs.find(man => man.manufacturer === car.manufacturerId);
+  const index = manufs.indexOf(found);
+  car.manufacturerId = index + 1;
+  return car;
+})
+
+// Create manuf obj array
+// const cleaned = cars.reduce((acc, car) => {
+//   acc[car.manufacturer] = null;
+//   return acc;
+// }, {});
 
 
-const manufs = Object.keys(cleaned).reduce((acc, car) => {
-  acc.push({ manufacturer: car })
-  return acc;
-}, []);
+// const manufs = Object.keys(cleaned).reduce((acc, car) => {
+//   acc.push({ manufacturer: car })
+//   return acc;
+// }, []);
 
-fs.writeFile('manufacturers.json', JSON.stringify(manufs), error => {throw error})
+fs.writeFile('carss.json', JSON.stringify(manufss), error => {throw error})
