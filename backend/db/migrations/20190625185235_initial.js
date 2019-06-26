cf6256b8da0b1d70ff04673fd62b35239750c48d
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-25 20:10:28 -0600

    Create schema for manufacturers

--- backend/db/migrations/20190625185235_initial.js
+++ backend/db/migrations/20190625185235_initial.js
@@ -1,6 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  // make manufacturers table
+  // make cars table
+
+  return Promise.all([
+    knex.schema.createTable('manufacturers', (table) => {
+      table.increments('id').primary();
+      table.string('manufacturer');
+
+      table.timestamps(true, true);
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
