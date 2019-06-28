f46e3b51a0b82dc0f5572fdff4ae86a9d31e8067
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-28 09:30:51 -0600

    Create production environment

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -12,6 +12,14 @@ module.exports = {
       directory: './seeds/dev'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './backend/db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
